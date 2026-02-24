1490f92c5f3cd02653eb172dbdd975f5078910ca
pjmlanders <pauldlanders@gmail.com>
2026-02-24 08:38:54 -0500

    debug: check env vars in Vercel function

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,11 @@ import type { IncomingMessage, ServerResponse } from 'http'
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({ status: 'ok', url: req.url }))
+  res.end(JSON.stringify({
+    status: 'ok',
+    url: req.url,
+    hasTM: !!process.env.TICKETMASTER_API_KEY,
+    hasAnth: !!process.env.ANTHROPIC_API_KEY,
+    nodeEnv: process.env.NODE_ENV,
+  }))
 }
